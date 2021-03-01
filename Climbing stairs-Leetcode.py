1540722b55cf7030e76cfb5197ac9940add710b8
KiranRaghavendra1248 <kiran.raghu.1248@gmail.com>
2021-03-01 22:18:24 +0530

    Added via Pycharm

--- Climbing stairs-Leetcode.py
+++ Climbing stairs-Leetcode.py
@@ -26,13 +26,13 @@ class Solution:
 # Top down approach solution
 class Solution:
     def climbStairs(self, n: int) -> int:
-        temp=[None for _ in range(n)]
-        for i in range(1,len(temp)+1):
+        temp=[None for _ in range(n+1)]
+        for i in range(1,n+1):
             if i==1:
-                temp[i-1]=1
+                temp[i]=1
             elif i==2:
-                temp[i-1]=2
+                temp[i]=2
             else:
-                temp[i-1]=temp[i-2]+temp[i-3]
-        return temp[n-1]
+                temp[i]=temp[i-1]+temp[i-2]
+        return temp[n]
 
