53bb4438320d5ada03c4083451331f0f08d17768
KiranRaghavendra1248 <kiran.raghu.1248@gmail.com>
2021-03-04 14:01:27 +0530

    Added via Pycharm

--- 0-1 Knapsack and Variations/0-1 Knapsack Top Down.py
+++ 0-1 Knapsack and Variations/0-1 Knapsack Top Down.py
@@ -7,7 +7,9 @@ def knapSack(W, wt, val, n):
                 temp[i][j] = 0
 
     # j signifies the weight that can be added/remaining wait in every iteration
-    # i signifies, the num of elements left to choose from
+    # i signifies, the first i elements to choose from
+    # So at any index temp[i][j], the value stored is the Maximum value, such that weight<=j
+    # And the max value has been attained using first i values
 
     ## Choice Diagram
     for i in range(1, n + 1):
@@ -15,5 +17,6 @@ def knapSack(W, wt, val, n):
             if wt[i - 1] > j:  # Do not include,as its bigger
                 temp[i][j] = temp[i - 1][j]
             elif wt[i - 1] <= j:  # Not bigger, but we have choice to include or not
-                temp[i][j] = max(val[i - 1] + temp[i - 1][j - wt[i]], temp[i - 1][j])
+                temp[i][j] = max(val[i - 1] + temp[i - 1][j - wt[i-1]], temp[i - 1][j])
     return temp[n][W]
+    # So temp[n][W] is the max value, such that capacity=W, which is attained by choosing from first n elements, i.e all elements
